867fc8f3821fd795139b8c5350386e55c8577e93
aferrar <aferrar90@gmail.com>
2016-11-19 07:43:27 -0500

    moved javascript to external file. created assets folder. started jquery for legend navigation

---
+++ application.js
@@ -0,0 +1,190 @@
+$(document).ready(function initMap() {
+
+  // Create a new StyledMapType object, passing it an array of styles,
+  // and the name to be displayed on the map type control.
+  var styledMapType = new google.maps.StyledMapType(
+      [
+        {elementType: 'geometry', stylers: [{color: '#ebe3cd'}]},
+        {elementType: 'labels.text.fill', stylers: [{color: '#523735'}]},
+        {elementType: 'labels.text.stroke', stylers: [{color: '#f5f1e6'}]},
+        {
+          featureType: 'administrative',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#c9b2a6'}]
+        },
+        {
+          featureType: 'administrative.land_parcel',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#dcd2be'}]
+        },
+        {
+          featureType: 'administrative.land_parcel',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#ae9e90'}]
+        },
+        {
+          featureType: 'landscape.natural',
+          elementType: 'geometry',
+          stylers: [{color: '#dfd2ae'}]
+        },
+        {
+          featureType: 'poi',
+          elementType: 'geometry',
+          stylers: [{color: '#dfd2ae'}]
+        },
+        {
+          featureType: 'poi',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#93817c'}]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'geometry.fill',
+          stylers: [{color: '#a5b076'}]
+        },
+        {
+          featureType: 'poi.park',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#447530'}]
+        },
+        {
+          featureType: 'road',
+          elementType: 'geometry',
+          stylers: [{color: '#f5f1e6'}]
+        },
+        {
+          featureType: 'road.arterial',
+          elementType: 'geometry',
+          stylers: [{color: '#fdfcf8'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry',
+          stylers: [{color: '#f8c967'}]
+        },
+        {
+          featureType: 'road.highway',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#e9bc62'}]
+        },
+        {
+          featureType: 'road.highway.controlled_access',
+          elementType: 'geometry',
+          stylers: [{color: '#e98d58'}]
+        },
+        {
+          featureType: 'road.highway.controlled_access',
+          elementType: 'geometry.stroke',
+          stylers: [{color: '#db8555'}]
+        },
+        {
+          featureType: 'road.local',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#806b63'}]
+        },
+        {
+          featureType: 'transit.line',
+          elementType: 'geometry',
+          stylers: [{color: '#dfd2ae'}]
+        },
+        {
+          featureType: 'transit.line',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#8f7d77'}]
+        },
+        {
+          featureType: 'transit.line',
+          elementType: 'labels.text.stroke',
+          stylers: [{color: '#ebe3cd'}]
+        },
+        {
+          featureType: 'transit.station',
+          elementType: 'geometry',
+          stylers: [{color: '#dfd2ae'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'geometry.fill',
+          stylers: [{color: '#17263c'}]
+        },
+        {
+          featureType: 'water',
+          elementType: 'labels.text.fill',
+          stylers: [{color: '#92998d'}]
+        }
+      ],
+      {name: 'Styled Map'});
+
+  // Create a map object, and include the MapTypeId to add
+  // to the map type control.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.748817, lng: -73.985428},
+    zoom: 12,
+    mapTypeControlOptions: {
+      mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
+              'styled_map']
+    }
+  });
+
+  var iconBase = 'assets/images/';
+  var icons = {
+    hunter: {
+      name: 'HUNTER COLLEGE',
+      icon: iconBase + 'hunter_marker.svg'
+    },
+    psu: {
+      name: 'PENN STATE UNIVERSITY',
+      icon: iconBase + 'psu_marker.svg'
+    },
+    ccny: {
+      name: 'CITY COLLEGE OF NY',
+      icon: iconBase + 'ccny_marker.svg'
+    },
+    hsmse: {
+      name: 'HS MATH,SCI,ENGINEERING' ,
+      icon: iconBase + 'hsmse_marker.svg'
+    }
+  };
+
+  function addMarker(feature) {
+    var marker = new google.maps.Marker({
+      position: feature.position,
+      icon: icons[feature.type].icon,
+      map: map
+    });
+  }
+
+
+  var features = [
+    {
+      position: new google.maps.LatLng(40.768599, -73.964729),
+      scaledSize: new google.maps.Size(25, 25),
+      type: 'hunter'
+    }, {
+      position: new google.maps.LatLng(40.8021458633399, -77.8670942478257),
+      scaledSize: new google.maps.Size(25, 25),
+      type: 'psu'
+    }, {
+      position: new google.maps.LatLng(40.819513, -73.950906),
+      scaledSize: new google.maps.Size(25, 25),
+      type: 'ccny'
+    }, {
+      position: new google.maps.LatLng(40.821587, -73.949516),
+      scaledSize: new google.maps.Size(50, 50),
+      type: 'hsmse'
+    }
+  ];
+
+  for (var i = 0, feature; feature = features[i]; i++) {
+    addMarker(feature);
+  }
+  //Associate the styled map with the MapTypeId and set it to display.
+  map.mapTypes.set('styled_map', styledMapType);
+  map.setMapTypeId('styled_map');
+});
+
+$(document).ready(function() {
+  $("#academic_buttton").click(function() {
+    $("#academic_title").hide();
+  }); 
+});
